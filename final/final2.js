a396b7e5778ad06431a7743059a34978104f3d17
Rabia-23 <rabiasevval2004@gmail.com>
2024-01-10 16:30:02 +0300

    Ağ Tabanlı Programlamaya Giriş dersi finali
    JavaScript, Css ve HTML kullanarak yaptığım; tür, yıl ve imdb puanı seçerek film posterleri gösteren site

---
+++ final/final2.js
@@ -0,0 +1,634 @@
+const movie0 = {
+   title : 'Shining',
+   director : 'Stanley Kubrick',
+   year : 1980,
+   duration : 146,
+   genre : ['horror', 'drama'],
+   country : "US",
+   imdb : 8.4
+};
+
+const movie1 = {
+   title : 'M3gan',
+   director : 'Gerard Johnstone',
+   year : 2022,
+   duration : 102,
+   genre : ['horror', 'sci-fi', 'thriller'],
+   country : "US",
+   imdb : 6.4
+};
+
+const movie2 = {
+   title : 'Pride and Prejudice',
+   director : 'Joe Wright',
+   year : 2005,
+   duration : 129,
+   genre : ['romance', 'drama'],
+   country : "UK",
+   imdb : 7.8
+};
+
+const movie3 = {
+   title : 'American Beauty',
+   director : 'Sam Mendes',
+   year : 1999,
+   duration : 122,
+   genre : ['drama'],
+   country : "US",
+   imdb : 8.3
+};
+
+const movie4 = {
+   title : 'The Hunger Games',
+   director : 'Gary Ross',
+   year : 2012,
+   duration : 142,
+   genre : ['action', 'adventure', 'sci-fi'],
+   country : "US",
+   imdb : 7.2
+};
+
+const movie5 = {
+   title : 'The Hunger Games - Catching Fire',
+   director : 'Francis Lawrence',
+   year : 2013,
+   duration : 146,
+   genre : ['action', 'adventure', 'sci-fi'],
+   country : "US",
+   imdb : 7.5
+};
+
+const movie6 = {
+   title : 'The Hunger Games - Mockingjay Part 1',
+   director : 'Francis Lawrence',
+   year : 2014,
+   duration : 123,
+   genre : ['action', 'adventure', 'sci-fi'],
+   country : "US",
+   imdb : 6.6
+};
+
+const movie7 = {
+   title : 'The Hunger Games - Mockingjay Part 2',
+   director : 'Francis Lawrence',
+   year : 2015,
+   duration : 137,
+   genre : ['action', 'adventure', 'sci-fi'],
+   country : "US",
+   imdb : 6.5
+};
+
+const movie8 = {
+   title : 'Persona',
+   director : 'Ingmar Bergman',
+   year : 1966,
+   duration : 83,
+   genre : ['drama', 'thriller'],
+   country : "Sweden",
+   imdb : 8.1
+};
+
+const movie9 = {
+   title : 'The Godfather',
+   director : 'Francis Ford Coppola',
+   year : 1972,
+   duration : 175,
+   genre : ['crime', 'drama'],
+   country : "US",
+   imdb : 9.2
+};
+
+const movie10 = {
+   title : 'The Godfather Part 2',
+   director : 'Francis Ford Coppola',
+   year : 1974,
+   duration : 202,
+   genre : ['crime', 'drama'],
+   country : "US",
+   imdb : 9.0
+};
+
+const movie11 = {
+   title : 'When Harry Met Sally',
+   director : 'Rob Reiner',
+   year : 1989,
+   duration : 95,
+   genre : ['comedy', 'drama', 'romance'],
+   country : "US",
+   imdb : 7.7
+};
+
+const movie12 = {
+   title : 'Interstellar',
+   director : 'Christopher Nolan',
+   year : 2014,
+   duration : 169,
+   genre : ['adventure', 'drama', 'sci-fi'],
+   country : "US",
+   imdb : 8.7
+}
+
+const movie13 = {
+   title : 'Inception',
+   director : 'Christopher Nolan',
+   year : 2010,
+   duration : 148,
+   genre : ['adventure', 'action', 'sci-fi'],
+   country : "US",
+   imdb : 8.8
+}
+
+const movie14 = {
+   title : 'Oppenheimer',
+   director : 'Christopher Nolan',
+   year : 2023,
+   duration : 180,
+   genre : ['biography', 'drama', 'history'],
+   country : "US",
+   imdb : 8.4
+}
+
+const movie15 = {
+   title : 'Barbie',
+   director : 'Greta Gerwig',
+   year : 2023,
+   duration : 114,
+   genre : ['adventure', 'comedy', 'fantasy'],
+   country : "US",
+   imdb : 7.0
+}
+
+const movie16 = {
+   title : 'Life is Beautiful',
+   director : 'Roberto Benigni',
+   year : 1997,
+   duration : 116,
+   genre : ['drama', 'comedy', 'romance'],
+   country : "Italy",
+   imdb : 8.6
+}
+
+const movie17 = {
+   title : 'Taken',
+   director : 'Pierre Morel',
+   year : 2008,
+   duration : 90,
+   genre : ['action', 'crime', 'thriller'],
+   country : "US",
+   imdb : 7.8
+}
+
+const movie18 = {
+   title : 'Die Hard',
+   director : 'John McTiernan',
+   year : 1988,
+   duration : 132,
+   genre : ['action', 'thriller'],
+   country : "US",
+   imdb : 8.2
+}
+
+const movie19 = {
+   title : 'Amélie',
+   director : 'Jean-Pierre Jeunet',
+   year : 2001,
+   duration : 122,
+   genre : ['comedy', 'romance'],
+   country : "France",
+   imdb : 8.3
+}
+
+const movie20 = {
+   title : 'Eternal Sunshine of the Spotless Mind',
+   director : 'Michel Gondry',
+   year : 2004,
+   duration : 108,
+   genre : ['drama', 'romance', 'sci-fi'],
+   country : "US",
+   imdb : 8.3
+}
+
+const movie21 = {
+   title : 'The Truman Show',
+   director : 'Peter Weir',
+   year : 1998,
+   duration : 103,
+   genre : ['drama', 'comedy'],
+   country : "US",
+   imdb : 8.2
+}
+
+const movie22 = {
+   title : 'Catch Me If You Can',
+   director : 'Steven Spielberg',
+   year : 2002,
+   duration : 141,
+   genre : ['biography', 'crime', 'drama'],
+   country : "US",
+   imdb : 8.1
+}
+
+const movie23 = {
+   title : 'Shutter Island',
+   director : 'Martin Scorsese',
+   year : 2010,
+   duration : 138,
+   genre : ['mystery', 'thriller'],
+   country : "US",
+   imdb : 8.2
+}
+
+const movie24 = {
+   title : 'Se7en',
+   director : 'David Fincher',
+   year : 1995,
+   duration : 127,
+   genre : ['mystery', 'drama', 'crime'],
+   country : "US",
+   imdb : 8.6
+}
+
+const movie25 = {
+   title : 'Pulp Fiction',
+   director : 'Quentin Tarantino',
+   year : 1994,
+   duration : 154,
+   genre : ['drama', 'crime'],
+   country : "US",
+   imdb : 8.9
+}
+
+const movie26 = {
+   title : 'Kill Bill - Vol.1',
+   director : 'Quentin Tarantino',
+   year : 2003,
+   duration : 111,
+   genre : ['action', 'crime', 'thriller'],
+   country : "US",
+   imdb : 8.2
+}
+
+const movie27 = {
+   title : 'Kill Bill - Vol.2',
+   director : 'Quentin Tarantino',
+   year : 2004,
+   duration : 137,
+   genre : ['action', 'crime', 'thriller'],
+   country : "US",
+   imdb : 8.0
+}
+
+const movie28 = {
+   title : 'Disaster Movie',
+   director : 'Jason Friedberg',
+   year : 2008,
+   duration : 87,
+   genre : ['comedy', 'sci-fi'],
+   country : "US",
+   imdb : 1.9
+}
+
+const movie29 = {
+   title : 'Manos - The Hands of Fate',
+   director : 'Quentin Tarantino',
+   year : 1966,
+   duration : 70,
+   genre : ['horror'],
+   country : "US",
+   imdb : 1.6
+}
+
+const movie30 = {
+   title : 'A Nightmare on Elm Street',
+   director : 'Wes Craven',
+   year : 1984,
+   duration : 91,
+   genre : ['horror'],
+   country : "US",
+   imdb : 7.4
+}
+
+const movie31 = {
+   title : 'Awakenings',
+   director : 'Penny Marshall',
+   year : 1990,
+   duration : 121,
+   genre : ['biography', 'drama'],
+   country : "US",
+   imdb : 7.8
+}
+
+const movie32 = {
+   title : 'A League of Their Own',
+   director : 'Penny Marshall',
+   year : 1992,
+   duration : 128,
+   genre : ['comedy', 'drama', 'sport'],
+   country : "US",
+   imdb : 7.3
+}
+
+const movie33 = {
+   title : 'A Bronx Tale',
+   director : 'Robert De Niro',
+   year : 1993,
+   duration : 121,
+   genre : ['crime', 'drama'],
+   country : "US",
+   imdb : 7.8
+}
+
+const movie34 = {
+   title : 'Angels in The Outfield',
+   director : 'William Dear',
+   year : 1994,
+   duration : 102,
+   genre : ['comedy', 'drama', 'family'],
+   country : "US",
+   imdb : 6.2
+}
+
+const movie35 = {
+   title : 'A Time to Kill',
+   director : 'Joel Schumacher',
+   year : 1996,
+   duration : 149,
+   genre : ['crime', 'drama', 'thriller'],
+   country : "US",
+   imdb : 7.5
+}
+
+const movie36 = {
+   title : 'The Midwife',
+   director : 'Marta Baidek',
+   year : 2021,
+   duration : 83,
+   genre : ['horror'],
+   country : "UK",
+   imdb : 3.8
+}
+
+const movie37 = {
+   title : 'Grimm',
+   director : 'Alex van Warmerdam',
+   year : 2003,
+   duration : 103,
+   genre : ['drama', 'adventure', 'comedy'],
+   country : "Netherlands",
+   imdb : 6.3
+}
+
+const movie38 = {
+   title : 'Death Warmed Up',
+   director : 'David Blyth',
+   year : 1984,
+   duration : 82,
+   genre : ['horror', 'sci-fi'],
+   country : "New Zealand",
+   imdb : 4.5
+}
+
+const movie39 = {
+   title : 'Moonwalkers',
+   director : 'Antoine Bardou-Jacquet',
+   year : 2015,
+   duration : 96,
+   genre : ['action', 'comedy'],
+   country : "France",
+   imdb : 6.1
+}
+
+const movie40 = {
+   title : 'Tucker - The Man and His Dream',
+   director : 'Francis Ford Coppola',
+   year : 1988,
+   duration : 110,
+   genre : ['biography', 'comedy', 'drama'],
+   country : "US",
+   imdb : 6.9
+}
+
+const movie41 = {
+   title : 'The Angel of Auschwitz',
+   director : 'Terry Lee Coker',
+   year : 2019,
+   duration : 101,
+   genre : ['drama', 'thriller', 'war'],
+   country : "UK",
+   imdb : 3.4
+}
+
+const movie42 = {
+   title : 'Gorky Park',
+   director : 'Michael Apted',
+   year : 1983,
+   duration : 128,
+   genre : ['crime', 'drama', 'mystery'],
+   country : "US",
+   imdb : 6.7
+}
+
+const movie43 = {
+   title : 'John G. Avildsen - King of the Underdogs',
+   director : 'Derek Wayne Johnson',
+   year : 2017,
+   duration : 98,
+   genre : ['documentary'],
+   country : "US",
+   imdb : 7.6
+}
+
+
+const movies = [movie0, movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9, movie10, movie11, movie12, movie13, movie14, movie15, movie16, movie17, movie18, movie19, movie20, movie21, movie22, movie23, movie24, movie25, movie26, movie27, movie28, movie29, movie30, movie31, movie32, movie33, movie34, movie35, movie36, movie37, movie38, movie39, movie40, movie41, movie42, movie43];   //array'a koymak disinda nasil bir yol izleyebilirim? Nasil yeni bir sayfada acabilirim?
+
+
+/*#######################################################################################*/
+
+
+///////////////////////////////////////////// GENRE LIST
+
+var genres = [];
+function selectedGenres(class_name)
+{
+
+   var existing = false;
+   var genre = document.querySelector(`.${class_name}`);
+
+   console.log("genres.length: " + genres.length);
+   console.log("existing: " + existing);
+   console.log("class_name: " + class_name);
+
+   for (let i = 0; i < genres.length; i++)
+   {
+      if (genres[i] === genre.value)
+      {
+         existing = true;
+
+         if (i === 0)
+         {
+            genres.shift();
+         }
+         else
+         {
+            genres.splice(i, i);
+         }
+      }
+   }
+
+   if (!(existing))
+   {
+      genres.push(genre.value);
+   }
+
+   console.log("existing: " + existing);
+
+   console.log("genres: " + genres);
+}
+
+
+////////////////////////////////////////// GENRE-TITLE LIST
+
+var counterG = 0;
+var titlesG = [];
+var tempTitleG = [];
+function titlesCWGenres()
+{
+   if (!(counterG === 0))
+   {
+      tempTitleG = titlesG;
+      titlesG = [];
+   }
+
+   for (let i = 0; i < genres.length; i++) //selected genre icinde geziyo
+   {
+      for (let j = 0; j < movies.length; j++) //movies icinde geziyo geziniyo
+      {
+         for (let k = 0; k < movies[j].genre.length; k++) //movie icindeki genre da geziyo
+         {
+            if (genres[i] === movies[j].genre[k] && !(titlesG.includes(movies[j].title)))
+            {
+               titlesG.push(movies[j].title);
+            }
+         }
+      }
+   }
+   counterG++;
+   console.log("Genre selection'undan sonra titlesG: " + titlesG);
+}
+
+
+////////////////////////////////////////// COUNTRY LIST
+
+
+
+
+////////////////////////////////////////////// COUNTRY-TITLE LIST
+
+
+
+
+////////////////////////////////////////////// YEAR LIST
+
+var titlesY = [];
+function titlesCWYears()
+{
+   titlesY = [];
+   titlesTempY = [];
+
+   var minYear = document.querySelector(".min-year");
+   var maxYear = document.querySelector(".max-year");
+
+   console.log("Year selectionun basinda titlesG: " + titlesG);
+
+
+   for (let i = 0; i < movies.length; i++)
+   {
+      if ((movies[i].year >= minYear.value) && (movies[i].year <= maxYear.value))
+      {
+         titlesY.push(movies[i].title);
+      }
+   }
+
+   console.log("titlesY: " + titlesY);
+   
+   for (let i = 0; i < titlesY.length; i++)
+   {
+      for (let j = 0; j < titlesG.length; j++)
+      {
+         if (titlesY[i] === titlesG[j])
+         {
+            titlesTempY.push(titlesG[j]);
+         }
+      }
+   }
+
+   titlesG = titlesTempY;
+
+   console.log("Year selectionundan sonra titlesG: " + titlesG);
+}
+
+
+//////////////////////////////////////////// IMDB LIST
+
+var titlesI = [];
+function titlesCWImdb()
+{
+   titlesI = [];
+
+   var minImdb = document.querySelector(".min-imdb");
+   var maxImdb = document.querySelector(".max-imdb");
+
+   if (minImdb.value > maxImdb.value)
+   {
+      minImdb.value = 0;
+      maxImdb.value = 10;
+   }
+   
+   for (let i = 0; i < movies.length; i++)
+   {
+      {
+         if (titlesG.includes(movies[i].title) && (minImdb.value <= movies[i].imdb && movies[i].imdb <= maxImdb.value))
+         {
+            titlesI.push(movies[i].title);
+         }
+      }
+   }
+
+   titlesG = titlesI;
+}
+
+
+//////////////////////////////////////////// POSTERS TO HTML
+
+function newPage()
+{
+   var ayirac = document.querySelector('.movie_grid');
+   console.log("counterG: " + counterG);
+
+   if (!(counterG === 1))   //fazladan kalacak posterleri onlemek icin
+   {
+      for (let i = 0; i < tempTitleG.length; i++)
+      {
+         var element = document.querySelector('.movie_card');
+         element.remove();
+      }
+   }
+
+   for (let i = 0; i < titlesG.length; i++)
+   {
+
+      var html = `<div class="movie_card_container">
+      <div class="movie_card">   <!--movie3-->
+                  
+            <div class="card_front_side">
+               <img class="poster" src="posters/${titlesG[i]}.jpg" alt="">
+            </div>
+      </div>
+   </div>`;
+
+      ayirac.insertAdjacentHTML('afterbegin', html);
+   }
+}
+
+
+//////////////////////////////////////////////////// EXPORT MOVIE NUM
